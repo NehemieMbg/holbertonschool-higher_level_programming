caf8e8d0d5b9c6be977c94b70c55fd40b2b85d11
NehemieMbg <nehemie.mbg@gmail.com>
2022-09-07 15:05:41 +0000

    prints in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# prints a string in uppercase followed by a new line
+def uppercase(str):
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - (ord('a') - ord('A')))
+        print("{:s}".format(c), end='')
+    print("")
