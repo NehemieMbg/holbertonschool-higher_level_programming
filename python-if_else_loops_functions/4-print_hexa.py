8f362430649bda0fe689c2c33b2485601b341efb
NehemieMbg <nehemie.mbg@gmail.com>
2022-09-07 11:57:51 +0000

    Prints number in decimals and hexadecimals

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # Prints number from 0-98 in decimals and in hexadecimals
 for i in range(0, 99):
-    print('{}'.format(i),'=','{}'.format(hex(i)))
+    print('{}'.format(i), '=', '{}'.format(hex(i)))
