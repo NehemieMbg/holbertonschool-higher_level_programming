1e554fc0e216d4f30a8fc82add08beb8f4fda1ec
NehemieMbg <nehemie.mbg@gmail.com>
2022-09-07 12:52:22 +0000

    Prints all number form 0-98

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # Prints all number from 0 to 98
 for i in range(99):
-    if i < 10:
-        print(0, end='')
-    elif i == 98:
-        print(98)
-    print('{}, '.format(i), end='')
+    if i > 0 and i < 99:
+        print(', ', end='')
+    print('0{}'.format(i) if i < 10 else '{}'.format(i), end='')
