274f3401330332577e918f25220e0147d267abe3
NehemieMbg <nehemie.mbg@gmail.com>
2022-09-08 14:24:08 +0000

    Checker test

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# prints a string in uppercase followed by a new line
+# converts a string in uppercase followed by a new line
 def uppercase(str):
     for character in str:
         if ord('a') <= ord(character) <= ord('z'):
