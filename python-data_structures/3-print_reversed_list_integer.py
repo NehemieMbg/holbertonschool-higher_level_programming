2ff3c6c316c37f5c9c795ab5beb37b02c0fad6f6
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-13 03:27:56 -0700

    Funciton that print in reversed

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None
-        return
     for index in range(len(my_list)-1, -1, -1):
         print('{:d}'.format(my_list[index]))
