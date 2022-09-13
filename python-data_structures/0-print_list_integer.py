75d38c19b8d227cf6e40560bfb1d2c54f05251f2
NehemieMbg <nehemie.mbg@gmail.com>
2022-09-13 01:33:11 -0700

    New funcition, Prints all integers

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pyhton
+# Function that prints all integers of a list.
+
+def print_list_integer(my_list=[]):
+    for index in range(0, len(my_list)):
+        print('{:d}'.format(my_list[index]))
