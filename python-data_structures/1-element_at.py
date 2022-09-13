b1af029f5b9cf40844476056ff0258f9fe5b6985
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-13 02:16:52 -0700

    Retrvies an element form a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,6 @@
 #function that retrieves an element from a list
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
-        return "None"
-    else:
+    if idx > 0 and idx < len(my_list):
         return my_list[idx]
+    return "None"
