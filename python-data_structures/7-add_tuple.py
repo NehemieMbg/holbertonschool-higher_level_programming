e1db9da2d7b8b20be8ef94589ff94be9e8209438
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-14 01:26:50 -0700

    Adds 2 tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,7 +3,9 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2: tuple_a += 0, 0
-    if len(tuple_b) < 2: tuple_b += 0, 0
+    if len(tuple_a) < 2:
+        tuple_a += 0, 0
+    if len(tuple_b) < 2:
+        tuple_b += 0, 0
     result = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return result
