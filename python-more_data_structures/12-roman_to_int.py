225a144959238d4b554d4560777c5a997cabbb29
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-17 07:51:57 -0700

    12-roman_to_int.py

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    lookup = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    num = len(roman_string)
+    i = num - 1
+    output = 0
+
+    while i >= 0:
+        if i < num - 1 and lookup[roman_string[i]]\
+                < lookup[roman_string[i + 1]]:
+            output -= lookup[roman_string[i]]
+        else:
+            output += lookup[roman_string[i]]
+        i -= 1
+    return output
