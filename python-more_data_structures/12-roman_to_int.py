acb63e342f5f98719ca3c77510ec5532551b9d84
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-17 08:04:10 -0700

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# Returns Roman numbers to Integer
 
 
 def roman_to_int(roman_string):
+    # Dictionary for the roman number
     lookup = {
         "I": 1,
         "V": 5,
@@ -11,19 +13,24 @@ def roman_to_int(roman_string):
         "D": 500,
         "M": 1000
     }
-
+    # If roman_string is not a string or doesn't exist
     if not isinstance(roman_string, str) or None:
         return 0
-
+    # num = the length of roman_string
     num = len(roman_string)
     i = num - 1
     output = 0
-
+    # Loop starting from the end
     while i >= 0:
+        # If the roman number at the index is bigger than
+        # the next index does a subtraction
         if i < num - 1 and lookup[roman_string[i]]\
                 < lookup[roman_string[i + 1]]:
             output -= lookup[roman_string[i]]
+        # Else does an addition
         else:
             output += lookup[roman_string[i]]
+        # Decrementation for looping in reverse
         i -= 1
+        # Return the Roman number to integers number
     return output
