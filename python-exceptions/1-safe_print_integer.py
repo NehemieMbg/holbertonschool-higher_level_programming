54864524c12190b8d93a14c792720c83e7b9a68d
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-18 08:22:51 -0700

    Prints integers

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,9 +6,8 @@ def safe_print_integer(value):
     # Will try to print an integer and return true
     # if value is an integer
     try:
-        for i in value:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     # Return false otherwise
     except:
         return False
