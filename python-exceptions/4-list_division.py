873066df110630bd97ec63c2bd6c3cd4c4ad01e6
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-18 10:50:34 -0700

    Devides element by element 2 lists

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Function that devides element by elment 2 lists
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    # Declaring a new list
+    newList = []
+    # Loop in list_length to devide the list1 by list2
+    for i in range(list_length):
+        # if the lists can be devided
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        # Handling erros if wrong type
+        except (ValueError, TypeError):
+            print("wrong type")
+            result = 0
+        # Handling erros if devided by zero
+        except ZeroDivisionError:
+            print("division by 0")
+        # Handling erros if out of range
+        except IndexError:
+            print("out of range")
+        # Pushes the result to the newList
+        finally:
+            newList.append(result)
+    # Returns the new list with number devided
+    return newList
