403779997dc5fdda79464fa06d01c8b40b4fa01c
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-18 09:37:04 -0700

    Raises a type exception

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+# Function that raises a type exception
+def raise_exception():
+    # To know more about exception:
+    # https://www.programiz.com/python-programming/exceptions
+    # Raised when a function or operation is applied to an
+    # object of incorrect type
+    raise TypeError()
