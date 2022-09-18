9ba0c73f7085b4f83a74539def0d65c660024fd9
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-18 09:31:42 -0700

    Raises a name exception with a message

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+# Function that Raises a name exception with a message
+def raise_exception_msg(message=""):
+    # To know more about exception:
+    # https://www.programiz.com/python-programming/exceptions
+    # Raised when a variable is not found in local or global scope.
+    raise NameError(message)
