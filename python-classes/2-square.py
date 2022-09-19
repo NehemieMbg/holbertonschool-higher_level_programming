94961d4e757ccda2e1ba888fe0971fc2e2b573d7
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-19 05:53:58 -0700

    Defines a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Class Square that defines a square """
+""" Defines a square """
 
 
 class Square:
-    """ Function """
+    """ defines a square """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
