0f633a594e13a6c812070e8fccfdd5c592bc3528
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-20 17:56:58 -0700

    defines a square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,6 +20,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = sier
 
     """ Returns the current square area """
     def area(self):
