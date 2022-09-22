23dbe75e64e159085ce9cca2e7c9193e26de0ad3
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-22 15:07:03 -0700

    Function that print names

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+"""
+Prints the first and last name
+"""
+
 
 def say_my_name(first_name, last_name=""):
-	# Check if thr first name is a string
-	if type(first_name) is not str:
-		raise TypeError("{:s} must be a string".format("first_name"))
-	# Check if the last name is a string
-	if type(last_name) is not str:
-		raise TypeError("{:s} must be a string".format("last_name"))
-	# If the first and last name are strings
-	else:
-		print("My name is {:s} {:s}".format(first_name, last_name))
+    """
+    Defines a function.
+    first_name: Takes the first name as an argument
+    last_name: Takes the last name as an argument
+    Arguments must be string type
+    """
+    # Check if thr first name is a string
+    if type(first_name) is not str:
+        """ Check if the fist name is a string """
+        raise TypeError("{:s} must be a string".format("first_name"))
+    # Check if the last name is a string
+    if type(last_name) is not str:
+        """ Check if the last name is a string """
+        raise TypeError("{:s} must be a string".format("last_name"))
+    # If the first and last name are strings
+    else:
+        """ Else printsMy name is <first name> <last name> """
+        print("My name is {:s} {:s}".format(first_name, last_name))
