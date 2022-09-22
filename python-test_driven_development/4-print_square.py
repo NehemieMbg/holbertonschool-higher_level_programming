0ef3f4fd8647ee7fe621adcf44800a0db16aeb8f
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-22 15:38:44 -0700

    Prints the square"

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Function that prints the square
+with the character #
+"""
+
+def print_square(size):
+    """
+    Defines a function
+    Takes an integer as an argument
+    """
+    # Check if size is an integer
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    # Check if size is greater than 0
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    # Check if size is not a float
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    # Prints the square with the character #
+    else:
+        print("\n".join("#"*size for rows in range(size)))
