1aa7ad59d015457a410be0ccb368de69351b0ec5
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-23 01:26:26 -0700

    Function that prints the text with new line when punctuation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Prints a text with 2 lines after
+each of these characters: ".", "?",":"
+"""
+
+
+def text_indentation(text):
+    """
+    Defines a function.
+    Takes a string as an argument
+    """
+    # Check if text is a string
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    # Prints 2 new lines after a punctuation
+    for char in text:
+        if char == "." or char == "?" or char == ":":
+            print(f"{char}\n")
+        else:
+            print(char, end="")
