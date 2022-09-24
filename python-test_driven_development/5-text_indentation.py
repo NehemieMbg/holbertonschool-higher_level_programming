5c72af69412ba4025648d02b00faf03b57a7b8a1
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-24 04:46:38 -0700

    Prints a text with 2 new lines after punctuation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,13 +10,25 @@ def text_indentation(text):
     Defines a function.
     Takes a string as an argument
     """
-    char = 0
     # Check if text is a string
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     # Prints 2 new lines after a punctuation
-    for char in text:
-        if char in [".", "?", ":"]:
-            print(f"{char}\n")
+    newText = text.replace(".", ".\n\n")
+    newText = newText.replace("?", "?\n\n")
+    newText = newText.replace(":", ":\n\n")
+    # Splits line when it encounters "\n"
+    parser = newText.splitlines(True)
+    print(parser)
+    # Declaring a new list
+    newList = []
+    # If index is a new line a new line is push to the newList
+    for index in parser:
+        if index == "\n":
+            newList.append("\n")
+    # else it pushes index to newlist with
+    # while removing the first space before the word
         else:
-            print(char, end="")
+            newList.append(index.lstrip())
+    # Joins the list back to a string
+    print("".join(newList), end="")
