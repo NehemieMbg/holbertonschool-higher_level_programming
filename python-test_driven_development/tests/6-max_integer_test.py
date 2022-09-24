f820a7e1bd1706a4d861cf881d84423dc3fb13f6
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-24 07:48:10 -0700

    Unittests for 6-max_integer

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -11,3 +11,25 @@ class TestMaxInteger(unittest.TestCase):
     def test_maxAtTheBeginning(self):
         """Test for max at the beginning"""
         self.assertEqual(max_integer([10, 4, 3, 1]), 10)
+    
+    def test_maxInTheMiddle(self):
+        """Test for max in the middle"""
+        self.assertEqual(max_integer([2, 3, 10, 5, 8]), 10)
+    
+    def test_oneNegativeNumber(self):
+        """Test for one negative number"""
+        self.assertEqual(max_integer([6, -3, 7, 8]), 8)
+    
+    def test_onlyNegativeNum(self):
+        """Test for only negative number"""
+        self.assertEqual(max_integer([-100, -85, -69, -24]), -24)
+    
+    def test_listOfElement(self):
+        """Test for list of element"""
+        elem = [10, 24, 36, 74]
+        self.assertEqual(max_integer(elem), 74)
+
+    def test_emptyList(self):
+        """Test for an empty list"""
+        elem = []
+        self.assertEqual(max_integer(elem), None)
