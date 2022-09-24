ce4f2844b158517a9ec0e94d28cda5d4b9e98baf
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-24 07:32:05 -0700

    Unittests for 6-max_integer

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,10 +5,10 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    """Test for max at the end"""
     def test_maxAtTheEnd(self):
-        self.assertEqual(test_MaxAtTheEnd([1, 2 , 5, 10], 10))
+        """Test for max at the end"""
+        self.assertEqual(max_integer([1, 2 , 5, 10], 10))
 
-    """Test for max at the beginning"""
     def test_maxAtTheBeginning(self):
-        self.assertEqual(test_maxAtTheBeginning([10, 4, 3, 1], 10))
+        """Test for max at the beginning"""
+        self.assertEqual(max_integer([10, 4, 3, 1], 10))
