ebb7ec69f292d5b39f6419bb7236d7f37631f061
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-25 05:59:12 -0700

    Defines a class Rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setter: Sets the width Value"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -34,7 +34,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setter: Sets the height Value"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
