947c278fbd2fd5c6ada70cce435fecf70b7f287f
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-25 06:01:55 -0700

    Defines a class Rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     Private Attribute: width, height
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -19,7 +19,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setter: Sets the width Value"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -34,7 +34,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setter: Sets the height Value"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
