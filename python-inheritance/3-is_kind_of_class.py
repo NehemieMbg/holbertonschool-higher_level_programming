050e1949186450b14703fc490a290b059ca0dd8c
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-27 16:07:27 -0700

    Return if isinstance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""3.Same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+	"""Function that Return if an object is an instance of"""
+	return isinstance(obj, a_class)
