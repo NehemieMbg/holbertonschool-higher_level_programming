22d86e8e8ded30029a42972b6583ab0fa6b49598
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-27 06:44:23 -0700

    Print a sorted list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""1.My list"""
+
+
+class MyList(list):
+    """
+    Class MyList
+    inherits list
+    """
+    def print_sorted(self):
+        """Function that print a sorted list"""
+        print(sorted(self))
