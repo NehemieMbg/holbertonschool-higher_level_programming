0c90aebc26f0fcdb5449ac0be5e472c838c28b03
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-27 16:30:47 -0700

    Raises an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""6. Improve Geometry"""
+
+
+class BaseGeometry:
+    """Defines a class"""
+    def area(self):
+        """Raises an exception with a message"""
+        raise Exception("area() is not implemented")
