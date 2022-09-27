dcbe6123658e45fb56eb703b5a4f5b682aef7287
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-27 07:16:46 -0700

    hot fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""1.My list"""
+"""Module 1 - My list"""
 
 
 class MyList(list):
-    """
-    Class MyList
-    inherits list
-    """
+    """inherits from list"""
+
     def print_sorted(self):
-        """Function that print a sorted list"""
+        """print the 'list', but sorted
+        ascending sort we mean"""
         print(sorted(self))
