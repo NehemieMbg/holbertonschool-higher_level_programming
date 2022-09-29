83a653717ad51fc2991d4032f5918a8b4eb2a017
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-29 06:42:11 -0700

    Returns an object Python data structure

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -3,5 +3,6 @@
 import json
 
 def from_json_string(my_str):
-	"""Returns an object Python data structure"""
-	return(json.loads(my_str))
+    """Returns an object Python data structure"""
+    return(json.loads(my_str))
+x
