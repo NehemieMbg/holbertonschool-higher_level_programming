17f219d51ee54c03882a3a3d9da393a68879aaf7
Nehemiembg <nehemie.mbg@gmail.com>
2022-09-30 02:30:08 -0700

    Adds arguments to a python list

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+from sys import argv
+"""7. Load, add, save"""
+"""Script that adds all arguments to a Python list,
+and save them to a file"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    fileContent = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    fileContent = []
+save_to_json_file(fileContent + (argv[1:]), "add_item.json")
