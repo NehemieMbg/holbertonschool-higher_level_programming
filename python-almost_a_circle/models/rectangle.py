ef65fc4106754d45ad692e8f99ec05f2c88f6453
Nehemiembg <nehemie.mbg@gmail.com>
2022-10-05 05:58:22 -0700

    returns the dictionary representation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -129,5 +129,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Returns the dictionary representation for a rectangle"""
-        return {"id": self.id, "width": self.width, \
+        return {"id": self.id, "width": self.width,
                 "height": self.height, "x": self.x, "y": self.y}
