dd43be5b96b887c375121a790913423840b29ae3
NehemieMbg <nehemie.mbg@gmail.com>
2022-11-03 16:09:41 +0100

    Removes all records with a score <= 5

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
 -- Removes all records with a score less than 6
-DELETE second_table.score, second_table.name, second_table.id
-FROM second_table WHERE score <= 5;
+DELETE FROM second_table
+WHERE score <= 5;
