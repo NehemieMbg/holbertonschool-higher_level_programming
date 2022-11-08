3beb63770c04805870692ac71de535c017096516
NehemieMbg <nehemie.mbg@gmail.com>
2022-11-08 17:00:13 +0100

    lists all shows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- Lists all shows contained in the table
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id ORDER BY id;
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
