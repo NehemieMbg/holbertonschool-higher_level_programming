51d35fcffd76bcf4fede9c9a3198213eee5662c0
NehemieMbg <nehemie.mbg@gmail.com>
2022-11-10 13:57:17 +0100

    display all values in the states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that list all states with name starting with N
+Takes in an argument and displays all values in the states
 """
 from sys import argv
 import MySQLdb
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         if row[1] == argv[4]:
