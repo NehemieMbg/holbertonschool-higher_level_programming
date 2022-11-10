3e5268683be5422051842d3722f99beb41682f8c
NehemieMbg <nehemie.mbg@gmail.com>
2022-11-10 14:34:44 +0100

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,12 +9,11 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute(
-        "SELECT cities.name FROM cities \
+    cur.execute("SELECT cities.name FROM cities \
                 INNER JOIN states WHERE states.id=cities.state_id \
-                AND states.name= %s ORDER BY cities.id ASC", (argv[4], )
-        )
-    query_rows = cur.fetchall()
+                AND states.name= %s ORDER BY cities.id ASC", (sys.argv[4], )
+                )
+    rows = cur.fetchall()
     display = (', '.join(row[0] for row in rows))
     print(str(display))
     cur.close()
