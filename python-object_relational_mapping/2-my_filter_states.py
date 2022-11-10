304eab8d3aaeb2c3f992cb4f83721b8722315850
NehemieMbg <nehemie.mbg@gmail.com>
2022-11-10 13:54:26 +0100

    display all values in the states

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that list all states with name starting with N
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = {argv[4]} ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    db.close()
