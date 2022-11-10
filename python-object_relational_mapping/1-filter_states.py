ae54d2d3b519a8087117407afaf9380543177c9d
NehemieMbg <nehemie.mbg@gmail.com>
2022-11-10 13:23:46 +0100

    list all state starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''script that lists all states from the database '''
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name="N%"ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
