47c3104a8c3bf9d67b7e5a08d5acca7f92d67d4c
NehemieMbg <nehemie.mbg@gmail.com>
2022-12-05 00:48:44 +0100

    Prints messages

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,5 +2,5 @@
 'use strict';
 
 if (process.argv.length === 1) console.log('No arguments');
-else if (process.argv.length === 1) console.log('Argument found');
+else if (process.argv.length === 2) console.log('Argument found');
 else console.log('Arguments found');
