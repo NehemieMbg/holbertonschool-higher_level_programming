6855e41849aa5cbf36aa930b955ed633df749263
NehemieMbg <nehemie.mbg@gmail.com>
2022-12-05 00:57:25 +0100

    Prints the first argument

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 'use strict';
 
-process.argv.length > 1 ? console.log(process.argv[2]) : console.log('No argument');
+process.argv[2] ? console.log(process.argv[2]) : console.log('No argument');
