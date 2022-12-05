3781c1b585e8ad91f62f15304b337f41d339c37d
NehemieMbg <nehemie.mbg@gmail.com>
2022-12-05 01:35:07 +0100

    prints time x

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 'use strict';
 
-const n = process.argv[2];
-
-if (n > 1) {
-	for (let i = 0; i < n; i++) {
+if (Number.isInteger(Number(process.argv[2]))) {
+	const n = process.argv[2];
+	for (let i = 0; i < Number(n); i++) {
 		console.log('C is fun');
 	}
-	return;
 }
 else console.log('Missing number of occurrences');
-return;
