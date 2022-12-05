2b1e2afb8dac951beddffe2065160533dc2101b4
NehemieMbg <nehemie.mbg@gmail.com>
2022-12-05 01:50:56 +0100

    Prints a square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,16 +3,15 @@
 
 const size = process.argv[2];
 
-function printSquare(num) {
-	if (!Number.isInteger(Number(num))) return console.log('Missing size');
+function printSquare (num) {
+  if (!Number.isInteger(Number(num))) return console.log('Missing size');
 
-	for (let i = 0; i < Number(num); i++) {
-		for (let j = 0; j < Number(num); j++) {
-			process.stdout.write('X');
-		}
-		console.log();
-	}
-	return;
+  for (let i = 0; i < Number(num); i++) {
+    for (let j = 0; j < Number(num); j++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
 }
 
 printSquare(size);
