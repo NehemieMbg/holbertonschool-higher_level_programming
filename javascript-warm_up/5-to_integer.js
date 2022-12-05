7964f94865e93fa73b87077526d3ff0b53980a5d
NehemieMbg <nehemie.mbg@gmail.com>
2022-12-05 01:07:25 +0100

    print if number the number

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 'use strict';
 
-const num = Number(process.argv[2]);
-typeof num === 'number' ? console.log(`My number: ${num}`) : console.log('Not a number');
+const num = process.argv[2];
+typeof +num === 'number' ? console.log(`My number: ${num}`) : console.log('Not a number');
